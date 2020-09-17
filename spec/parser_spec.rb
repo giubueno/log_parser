d34ad509a617eb9128f72f015c718f125f4ef543
Giulliano Bueno <giubueno@gmail.com>
2020-09-17 21:38:30 +0000

    More test cases.

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -6,5 +6,17 @@ describe Parser do
     it 'returns the accessed path' do
       expect(Parser::parse_line('/about 722.247.931.582')).to eq('/about')
     end
+
+    context 'when the line is blank' do
+      it 'returns nil' do
+        expect(Parser::parse_line('')).to be_nil
+      end
+    end
+
+    context 'when the line has only spaces' do
+      it 'returns nil' do
+        expect(Parser::parse_line('   ')).to be_nil
+      end
+    end
   end
 end
