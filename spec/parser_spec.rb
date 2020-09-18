0f4548db9a69b47c43ae727278f1a7d43c8cb44f
Giulliano Bueno <giubueno@gmail.com>
2020-09-18 00:51:35 +0000

    Few code style fixes.

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -5,32 +5,28 @@ require_relative '../parser'
 
 describe Parser do
   describe '::parse_line' do
-    subject { Parser::parse_line(line) }
+    subject(:total) { Parser::parse_line(line) }
 
     let(:line) { '/about 722.247.931.582' }
 
     it 'returns a struct with the accessed path' do
-      expect(subject.path).to eq('/about')
+      expect(total.path).to eq('/about')
     end
 
     it 'returns a struct with the ip' do
-      expect(subject.ip).to eq('722.247.931.582')
+      expect(total.ip).to eq('722.247.931.582')
     end
 
     context 'when the line is blank' do
       let(:line) { '' }
 
-      it 'returns nil' do
-        expect(subject).to be_nil
-      end
+      it { should be_nil }
     end
 
     context 'when the line has only spaces' do
       let(:line) { '' }
 
-      it 'returns nil' do
-        expect(subject).to be_nil
-      end
+      it { should be_nil }
     end
   end
 
@@ -39,33 +35,33 @@ describe Parser do
     let(:line) { '/about 722.247.931.582' }
 
     describe '.add' do
-      subject { counter.add(line) }
+      subject(:total) { counter.add(line) }
 
       it 'returns an struct with the number of non-unique access' do
-        expect(subject.non_unique).to eq(1)
+        expect(total.non_unique).to eq(1)
       end
 
       it 'returns an struct with the number of unique accesses' do
-        expect(subject.unique).to eq(1)
+        expect(total.unique).to eq(1)
       end
 
       context 'when the same line is added' do
-        subject do
+        subject(:total) do
           counter.add(line)
           counter.add(line)
         end
 
         it 'returns only one unique access' do
-          expect(subject.unique).to eq(1)
+          expect(total.unique).to eq(1)
         end
 
         it 'returns two non-unique access' do
-          expect(subject.non_unique).to eq(2)
+          expect(total.non_unique).to eq(2)
         end
       end
 
       context 'when two different lines are added' do
-        subject do
+        subject(:total) do
           counter.add(line)
           counter.add(other_line)
         end
@@ -73,11 +69,11 @@ describe Parser do
         let(:other_line) { '/home 722.247.931.582' }
 
         it 'returns only one unique access' do
-          expect(subject.unique).to eq(1)
+          expect(total.unique).to eq(1)
         end
 
         it 'returns only one non-unique access' do
-          expect(subject.non_unique).to eq(1)
+          expect(total.non_unique).to eq(1)
         end
       end
     end
@@ -96,14 +92,14 @@ describe Parser do
       end
 
       it 'returns a sorted hash (descending and ordered by unique accesses)' do
-        expect(subject).to eq(['/home/index', '/home'])
+        should eq(['/home/index', '/home'])
       end
 
       context 'when sort is passed as asc' do
         subject { counter.totals(:unique, :asc).map(&:first) }
 
         it 'returns a sorted hash (ascending and ordered by unique accesses)' do
-          expect(subject).to eq(["/home", "/home/index"])
+          should eq(['/home', '/home/index'])
         end
       end
     end
