1a07f2ece404bbf059be8772c3ae58761fc26cf0
Giulliano Bueno <giubueno@gmail.com>
2020-09-21 01:04:22 +0200

    Small spec name improvement.

--- spec/services/parsers/line_parser_spec.rb
+++ spec/services/parsers/line_parser_spec.rb
@@ -6,7 +6,7 @@ require_relative '../../../app/services/parsers/line_parser'
 describe Services::Parsers::LineParser do
   subject(:parser) { described_class.new }
 
-  describe '.parse' do
+  describe '#parse' do
     let(:access) { parser.parse('/about 722.247.931.582') }
 
     it { expect(access.path).to eq('/about') }
