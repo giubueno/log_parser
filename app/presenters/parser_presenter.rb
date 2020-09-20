799a044a5a00df6ffefda166f0a18e55073d9b88
Giulliano Bueno <giubueno@gmail.com>
2020-09-21 01:08:33 +0200

    Add presenter logic

--- app/presenters/parser_presenter.rb
+++ app/presenters/parser_presenter.rb
@@ -1,6 +1,25 @@
 # frozen_string_literal: true
 
+require_relative '../exceptions/parser_exception'
+
 module Presenters
+  # Presenter responsible to coordinate the parsing process.
   class ParserPresenter
+    def initialize(parser_service, error_view, result_view)
+      @error_view = error_view
+      @result_view = result_view
+      @parser_service = parser_service
+    end
+
+    def start
+      totals = parser_service.parse
+      result_view.render(totals)
+    rescue Parser::ParserException => e
+      error_view.render(e)
+    end
+
+    private
+
+    attr_accessor :error_view, :result_view, :parser_service
   end
 end
