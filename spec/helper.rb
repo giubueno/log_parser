916e85a3c7b0234f2a579f8d5d31bc8592ef3f66
Giulliano Bueno <giubueno@gmail.com>
2020-09-21 04:16:58 +0200

    Add track

--- spec/helper.rb
+++ spec/helper.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 require 'simplecov'
-SimpleCov.start do
-  track_files "/app/**/*.rb"
-end
 
 if ENV['CI'] == 'true'
   require 'codecov'
   SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
+
+SimpleCov.start do
+  track_files '/app/**/*.rb'
 end
