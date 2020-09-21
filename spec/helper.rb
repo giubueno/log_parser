16770b05478a9ea00b43283f7dcf1e511bc74028
Giulliano Bueno <giubueno@gmail.com>
2020-09-21 04:23:48 +0200

    Codecov

--- spec/helper.rb
+++ spec/helper.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
 require 'simplecov'
-require 'codecov'
-SimpleCov.formatter = SimpleCov::Formatter::Codecov
+
+if ENV['CI'] == 'true'
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
+
 SimpleCov.start do
   add_filter '/spec/'
 end
